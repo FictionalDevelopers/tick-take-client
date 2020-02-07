1708cf6a31c30b17de856cdc685ec25ab5f243ea
YarikLysak <zhbrkscfr@gmail.com>
2020-02-07 14:50:50 +0200

    registration page layout

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  isLogin: boolean;
   loginForm: FormGroup;
-  constructor(private fb: FormBuilder) {
-    this.loginForm = this.fb.group({
-      email: '',
-      password: ''
+  title: string;
+
+  constructor(private route: ActivatedRoute, private fb: FormBuilder) {
+    this.route.queryParams.subscribe(({ type }) => {
+      console.log(type);
+
+      this.isLogin = type === 'login';
+      this.title = type;
+      if (this.isLogin) {
+        this.loginForm = this.fb.group({
+          email: '',
+          password: ''
+        });
+      } else {
+        this.loginForm = this.fb.group({
+          name: '',
+          email: '',
+          password: '',
+          confirmPass: ''
+        });
+      }
     });
   }
+
+  ngOnInit() {}
 }
