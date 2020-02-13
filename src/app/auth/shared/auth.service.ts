2314036f1e2ef37ba1a095a5bef283134008d572
YarikLysak <zhbrkscfr@gmail.com>
2020-02-13 11:35:58 +0200

    proxy config

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -2,10 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { SERVER_ULR, HEADERS } from '../../shared/constants';
 import { User } from './user.model';
-
-const AUTH_URL = `${SERVER_ULR}/auth`;
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +13,7 @@ export class AuthService {
   //   return this.http.post();
   // }
   login(loginData): Observable<string> {
-    return this.http.post<string>(`${AUTH_URL}/login`, { headers: HEADERS, params: loginData });
+    return this.http.post<string>(`api/auth/login`, { params: loginData });
   }
 
   constructor(private http: HttpClient) {}
--- src/app/shared/constants.ts
+++
@@ -1,2 +0,0 @@
-export const SERVER_ULR = 'http://localhost:5000/api';
-export const HEADERS = new Headers({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
